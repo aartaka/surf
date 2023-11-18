30f5464eb11b96f740b124816cbcfa55f125cf53
Quentin Rameau <quinq@fifth.space>
2023-11-18 12:40:11 +0100

    Fix usage message: -Pp don't exist anymore
    Thanks to Eric Pruitt <eric.pruitt@gmail.com> for reporting!

--- surf.c
+++ surf.c
@@ -322,7 +322,7 @@ die(const char *errstr, ...)
 void
 usage(void)
 {
-	die("usage: surf [-bBdDfFgGiIkKmMnNpPsStTvwxX]\n"
+	die("usage: surf [-bBdDfFgGiIkKmMnNsStTvwxX]\n"
 	    "[-a cookiepolicies ] [-c cookiefile] [-C stylefile] [-e xid]\n"
 	    "[-r scriptfile] [-u useragent] [-z zoomlevel] [uri]\n");
 }
