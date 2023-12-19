56b8257868de9c1f06a66464152766696212cf66
Artyom Bologov <aartaka@protonmail.com>
2023-12-19 16:50:16 +0400

    config: Use Disroot as home page.

--- config.def.h
+++ config.def.h
@@ -202,4 +202,4 @@ static Button buttons[] = {
 	{ OnMedia,      MODKEY,         1,      clickexternplayer, { 0 },       1 },
 };
 
-#define HOMEPAGE "https://duckduckgo.com/"
+#define HOMEPAGE "https://apps.disroot.org/"
