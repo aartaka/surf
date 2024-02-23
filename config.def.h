9922ef6343f9e22fdbe8a133707855a2ea9c6edb
Artyom Bologov <aartaka@protonmail.com>
2024-02-24 00:10:36 +0400

    Use default zoom.

--- config.def.h
+++ config.def.h
@@ -53,7 +53,7 @@ static Parameter defconfig[ParameterLast] = {
 	[StrictTLS]           =       { { .i = 1 },     },
 	[Style]               =       { { .i = 1 },     },
 	[WebGL]               =       { { .i = 0 },     },
-	[ZoomLevel]           =       { { .f = 1.3 },   },
+	[ZoomLevel]           =       { { .f = 1.0 },   },
 };
 
 static UriParameters uriparams[] = {
